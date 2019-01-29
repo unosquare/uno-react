93060c9950214f4f1ee1b785831ee26eaa3b8495
Geo Perez <geovanni.perez@gmail.com>
2019-01-29 13:52:22 -0600

    0.0.19

--- src/hooks/index.js
+++ src/hooks/index.js
@@ -1,5 +1,6 @@
 export * from './useStateForField';
 export * from './useStateForModel';
+export * from './useStateForModelWithLoading';
 export * from './useEffectWithLoading';
 export * from './usePersistedState';
 export * from './useToggleStateForField';
