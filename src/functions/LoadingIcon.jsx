887a5a5b900b3201c67263d50fe06d9c1c9a0239
Geo Perez <geovanni.perez@gmail.com>
2019-01-30 10:06:05 -0600

    Change to theme

--- src/functions/LoadingIcon.jsx
+++ src/functions/LoadingIcon.jsx
@@ -1,9 +1,9 @@
 import CircularProgress from '@material-ui/core/CircularProgress';
 import { styled } from '@material-ui/styles';
 
-export const LoadingIcon = styled(CircularProgress)({
-    color: '#165ffb',
+export const LoadingIcon = styled(CircularProgress)(theme => ({
+    color: theme.palette.primary.main,
     display: 'block !important',
     margin: 'auto !important',
     marginBottom: '15px !important',
-});
+}));
