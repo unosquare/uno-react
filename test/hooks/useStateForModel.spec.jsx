e26a3e543e3adac01fb4c52d10b1b75150633bd8
AnyxLeo <anyx.leo@gmail.com>
2019-02-11 10:00:53 -0600

    WIP

--- test/hooks/useStateForModel.spec.jsx
+++ test/hooks/useStateForModel.spec.jsx
@@ -5,10 +5,8 @@ import createShallow from '@material-ui/core/test-utils/createShallow';
 
 describe('useStateForModel', () => {
     let shallow;
-    const [ model, handleChange ] = useStateForModel({
-        id: 1,
-        name: 'Mario Di Vece'
-    });
+    let model = { id: 1, name: 'Mario Di Vece' };
+    const handleChange = jest.fn();    
 
     beforeEach(() => {
         jest.resetModules();      
@@ -17,16 +15,18 @@ describe('useStateForModel', () => {
    
 
     test('Should the model change to {id: 1, name: Mario Di Vece}', () => {
-        handleChange( { name: 'Geo Perez' } );
+        handleChange.mockReturnValue({ id: 1, name: 'Geo Perez' })();
+        model = handleChange();
         expect(model.name).toBe('Geo Perez');
         expect(model.id).toBe(1);
     });
 
-    expect('Should respond to onChange event', () => {
-        const wrapper = shallow(<TextField onChange={handleChange}/>);
-        const input = wrapper.find('input');
-        input.simulate('change', { target: { name: 'name', value: 'Ana Lizardi' } });
-
-        expect(model.name).toBe('Ana Lizardi');
+    test('Should respond to onChange event', () => {
+        const wrapper = shallow(<input name = 'name' value={model.name} onChange={handleChange}/>);
+        const input = wrapper.find({ name: 'name' });
+        input.simulate('change', { target: { value: 'Ana Lizardi' } });
+        // console.log(wrapper.debug());
+        console.log(input.debug());
+        expect(input.value).toBe('Ana Lizardi');
     });
 });
