7fce68db89407c6c2d108a78795bf1f772a29a31
Leonardi Pérez <marcolpr98@gmail.com>
2019-02-12 15:17:30 -0600

    Changed GlobalSnackbar

--- src/classes/GlobalSnackbar.jsx
+++ src/classes/GlobalSnackbar.jsx
@@ -89,3 +89,90 @@ const GlobalSnackbar = ({ message, type, open, onClose, autoHideTimeout }) => (
 );
 
 export default GlobalSnackbar;
+
+
+import amber from '@material-ui/core/colors/amber';
+import green from '@material-ui/core/colors/green';
+import Snackbar from '@material-ui/core/Snackbar';
+import SnackbarContent from '@material-ui/core/SnackbarContent';
+import Typography from '@material-ui/core/Typography';
+import CheckCircle from '@material-ui/icons/CheckCircle';
+import Error from '@material-ui/icons/Error';
+import Info from '@material-ui/icons/Info';
+import Warning from '@material-ui/icons/Warning';
+import { makeStyles } from '@material-ui/styles';
+import React, { useEffect, useState } from 'react';
+
+const useStyles = makeStyles((theme) => ({
+    error: {
+        backgroundColor: theme.palette.secondary.main,
+    },
+    icon: {
+        fontSize: '34px',
+        marginRight: '15px',
+    },
+    info: {
+        backgroundColor: theme.palette.primary.main,
+    },
+    success: {
+        backgroundColor: green[600],
+    },
+    text: {
+        color: theme.palette.text.secondary,
+        display: 'inline-flex',
+        fontSize: '28px',
+    },
+    warning: {
+        backgroundColor: amber[700],
+    },
+}));
+
+const GlobalSnackbar = ({ message, messageType, seconds = 2500 }) => {
+    const classes = useStyles();
+    const [open, setOpen] = useState(false);
+
+    const getIcon = () => {
+        switch (messageType) {
+            case ('info'): return <Info className={classes.icon} />;
+            case ('warning'): return <Warning className={classes.icon} />;
+            case ('sucess'): return <CheckCircle className={classes.icon} />;
+            case ('error'): return <Error className={classes.icon} />;
+        }
+    };
+    const getStyle = () => {
+        switch (messageType) {
+            case ('info'): return classes.info;
+            case ('warning'): return classes.warning;
+            case ('sucess'): return classes.success;
+            case ('error'): return classes.error;
+        }
+    };
+
+    useEffect(() => {
+        if (message !== '') {
+            setOpen(true);
+            setTimeout(() => {
+                setOpen(false);
+            }, seconds);
+        }
+    }, [message]);
+
+    return (
+        <Snackbar
+            className={getStyle()}
+            open={open}
+        >
+            <SnackbarContent
+                className={getStyle()}
+                message={
+                    <React.Fragment>
+                        <Typography className={classes.text}> {getIcon()} {message} </Typography>
+                    </React.Fragment>
+                }
+            />
+        </Snackbar>
+    );
+};
+
+export default GlobalSnackbar;
+
