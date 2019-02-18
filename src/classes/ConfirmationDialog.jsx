8e4b7cc603ef6f6c80229647b215003fd2f49431
JCorrea13 <josemanuelcorrea.13@gmail.com>
2019-02-18 10:57:56 -0600

    TextColor in Confirmation Dialog

--- src/classes/ConfirmationDialog.jsx
+++ src/classes/ConfirmationDialog.jsx
@@ -6,7 +6,7 @@ import DialogContent from '@material-ui/core/DialogContent';
 import DialogContentText from '@material-ui/core/DialogContentText';
 import DialogTitle from '@material-ui/core/DialogTitle';
 
-const ConfirmationDialog = ({ contentText, title, onAgreeAction, onClose, open, agree = 'Agree', disagree = 'Disagree', textColor= 'secondary' | 'textSecondary' }) => {
+const ConfirmationDialog = ({ contentText, title, onAgreeAction, onClose, open, agree = 'Agree', disagree = 'Disagree', textColor= 'textSecondary' | 'secondary' }) => {
     const handleAgree = e => onAgreeAction(e);
 
     return (
