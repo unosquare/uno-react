29a6179e827cc8398985bbe5333406b281403cbf
Leonardi Pérez <marcolpr98@gmail.com>
2019-02-19 11:52:46 -0600

    Fixed buttons styles and fixed lint

--- src/functions/humanize.js
+++ src/functions/humanize.js
@@ -1,3 +1,3 @@
 export function humanize(name) {
-    return (name || '').replace(/^./, (str) => str.toUpperCase()).replace(/([a-z])([A-Z])/g, '$1 $2');
+    return (name || '').replace(/^./, str => str.toUpperCase()).replace(/([a-z])([A-Z])/g, '$1 $2');
 }
