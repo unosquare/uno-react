2400927aab98ec2da183ea53ccb7e53f8a893599
Fernando Leon <fernando.leon@unosquare.com>
2019-02-20 14:03:09 -0600

    - GlobalSnackbar Test moved to folder Functions. - add call to create classes into props in order to make the function testable.

--- src/functions/GlobalSnackbar.jsx
+++ src/functions/GlobalSnackbar.jsx
@@ -53,8 +53,7 @@ const useStyles = makeStyles(theme => ({
     },
 }));
 
-const GlobalSnackbar = ({ message, seconds = 2500, mobile = false }) => {
-    const classes = useStyles();
+const GlobalSnackbar = ({ message, seconds = 2500, mobile = false, classes = useStyles() }) => {
     const [open, setOpen] = useState(false);
 
     const getIcon = () => {
---
+++ test/functions/CustomSnackbarContent.spec.jsx
@@ -0,0 +1,26 @@
+import * as React from 'react';
+import createShallow from '@material-ui/core/test-utils/createShallow';
+import GlobalSnackbar from '../../src/functions/GlobalSnackbar'
+import Snackbar from '@material-ui/core/Snackbar';
+import SnackbarContent from '@material-ui/core/SnackbarContent';
+
+describe('CustomSnackBarContent', () => {
+    let wrapper;
+    let shallow;
+    beforeEach(() => {
+        jest.resetModules();
+        shallow = createShallow();
+        wrapper = shallow(<GlobalSnackbar
+            message='test message'
+            classes={{}}
+        />);
+    });
+
+    test('Should contain a <Snackbar />', () => {
+        expect(wrapper.find(Snackbar)).toHaveLength(1);
+    });
+
+    test('Should contain a <SnackbarContent />', () => {
+        expect(wrapper.find(SnackbarContent)).toHaveLength(1);
+    });
+});
