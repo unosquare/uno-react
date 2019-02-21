fba191c891af18af9f7c57fdd28f69375ac49caa
Geo Perez <1775792+geoperez@users.noreply.github.com>
2019-02-21 14:48:54 -0600

    Update deps

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,18 +1,22 @@
+import { Theme } from '@material-ui/core/styles/createMuiTheme';
+import React from 'react';
 
-export const AppTitle: any;
+export type Procedure = (...args: any[]) => void;
+
+export const AppTitle: (prefix: string, suffix: string) => React.FunctionComponent;
 export const ButtonWithLoading: any;
 export const ConfirmationDialog: any;
 export const ErrorBoundary: any;
 export const GlobalSnackbar: any;
 export const LoadingIcon: any;
 export const TextValidator: any;
-export const Title: any;
+export const Title: React.FunctionComponent;
 export const asyncOnEnterKey: any;
 export const colorGenerator: any;
 export const createFetchController: any;
 export const debounce: any;
 export const enhanceWithClickOutside: any;
-export const humanize: any;
+export const humanize: (name: string) => string;
 export const isNotAllBlanks: any;
 export const maxNaturalNumber: any;
 export const onEnterKey: any;
@@ -23,5 +27,6 @@ export const usePersistedState: any;
 export const useStateForField: any;
 export const useStateForModel: any;
 export const useStateForModelWithLoading: any;
-export const useToggle: any;
-export const useToggleStateForField: any;
+export const useToggle: (defaultValue: any) => any;
+export const useToggleStateForField: (defaultValue: any) => any;
+export const UnoTheme: Theme;
