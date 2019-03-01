5c73bb3e18ec74ed4e265bb8c20ae3a7dab3fbc0
Geo Perez <1775792+geoperez@users.noreply.github.com>
2019-03-01 11:03:50 -0600

    Typescript

---
+++ lib/index.d.ts
@@ -0,0 +1,32 @@
+import { Theme } from '@material-ui/core/styles/createMuiTheme';
+import React from 'react';
+
+export type Procedure = (...args: any[]) => void;
+
+export const AppTitle: (prefix: string, suffix: string) => React.FunctionComponent;
+export const ButtonWithLoading: any;
+export const ConfirmationDialog: any;
+export const ErrorBoundary: any;
+export const GlobalSnackbar: any;
+export const LoadingIcon: any;
+export const TextValidator: any;
+export const Title: React.FunctionComponent;
+export const asyncOnEnterKey: any;
+export const colorGenerator: any;
+export const createFetchController: any;
+export const debounce: any;
+export const enhanceWithClickOutside: any;
+export const humanize: (name: string) => string;
+export const isNotAllBlanks: any;
+export const maxNaturalNumber: any;
+export const onEnterKey: any;
+export const requestController: any;
+export const useEffectWithLoading: any;
+export const useNow: any;
+export const usePersistedState: any;
+export const useStateForField: any;
+export const useStateForModel: any;
+export const useStateForModelWithLoading: any;
+export const useToggle: (defaultValue: any) => any;
+export const useToggleStateForField: (defaultValue: any) => any;
+export const UnoTheme: Theme;
