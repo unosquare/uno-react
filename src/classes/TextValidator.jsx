7a09d945a642c3847f997f0a8fe1d42c5dc44257
Leonardi Pérez <marcolpr98@gmail.com>
2019-03-01 16:32:19 -0600

    Added funcionality (#44)
    * Added funcionality
    
    If there is no label, it takes the id and humanize it
    
    * Fix

--- src/classes/TextValidator.jsx
+++ src/classes/TextValidator.jsx
@@ -1,6 +1,7 @@
 /* eslint-disable no-unused-vars */
 import TextField from '@material-ui/core/TextField';
 import * as React from 'react';
+import humanize from '../functions/humanize';
 
 import { ValidatorComponent } from 'react-form-validator-core';
 
@@ -13,12 +14,15 @@ export default class TextValidator extends ValidatorComponent {
             helperText,
             validatorListener,
             withRequiredValidator,
+            label,
+            id,
             ...rest } = this.props;
         const { isValid } = this.state;
 
         return (
             <TextField
                 {...rest}
+                label={label || humanize(id)}
                 error={!isValid || error}
                 helperText={(!isValid && this.getErrorMessage()) || helperText}
             />
