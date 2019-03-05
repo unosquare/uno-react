987f2c2b35b1f0a795ef975b90dd64c33632e865
Geo Perez <1775792+geoperez@users.noreply.github.com>
2019-03-05 14:53:58 -0600

    Fix loading icon export

--- src/components/index.js
+++ src/components/index.js
@@ -2,7 +2,7 @@ export { default as ButtonWithLoading } from './ButtonWithLoading';
 export { default as CentralCard } from './CentralCard';
 export * from './ConfirmationDialog';
 export { default as GlobalSnackbar } from './GlobalSnackbar';
-export { default as LoadingIcon } from './LoadingIcon';
+export * from './LoadingIcon';
 export { default as MenuList } from './MenuList';
 export { default as NavBar } from './NavBar';
 export * from './Title';
