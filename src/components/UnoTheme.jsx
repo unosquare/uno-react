fc8d727529e9297fc25d289e9fb787639fcceb8f
Leonardi Pérez <marcolpr98@gmail.com>
2019-03-06 16:11:15 -0600

    Added override to form label

--- src/components/UnoTheme.jsx
+++ src/components/UnoTheme.jsx
@@ -1,25 +1,32 @@
 import createMuiTheme from '@material-ui/core/styles/createMuiTheme';
 
 export const UnoTheme = createMuiTheme({
-    palette: {
-      error: {
-        main: '#f44336',
-      },
-      primary: {
-        main: '#304FF3',
-      },
-      secondary: {
-        main: '#5c5b5b',
-      },
-      text: {
-        primary: '#191919',
-        secondary: '#FFF',
+  overrides: {
+    MuiFormLabel: {
+      root: {
+        color: '#191919',
       },
     },
-    typography: {
-      useNextVariants: true,
+  },
+  palette: {
+    error: {
+      main: '#f44336',
     },
-    zIndex: {
-      appBar: '1201 !important'
+    primary: {
+      main: '#304FF3',
     },
+    secondary: {
+      main: '#5c5b5b',
+    },
+    text: {
+      primary: '#191919',
+      secondary: '#FFF',
+    },
+  },
+  typography: {
+    useNextVariants: true,
+  },
+  zIndex: {
+    appBar: '1201 !important'
+  },
 });
