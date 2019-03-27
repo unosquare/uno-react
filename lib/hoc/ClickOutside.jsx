a187a821b067dfc0b2bd4b23c5fed6929bd0b2bf
Geo Perez <1775792+geoperez@users.noreply.github.com>
2019-03-27 13:12:13 -0600

    Update to ES6

--- lib/hoc/ClickOutside.jsx
+++ lib/hoc/ClickOutside.jsx
@@ -1,16 +1,3 @@
-var __extends = (this && this.__extends) || (function () {
-    var extendStatics = function (d, b) {
-        extendStatics = Object.setPrototypeOf ||
-            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
-            function (d, b) { for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p]; };
-        return extendStatics(d, b);
-    };
-    return function (d, b) {
-        extendStatics(d, b);
-        function __() { this.constructor = d; }
-        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
-    };
-})();
 var __rest = (this && this.__rest) || function (s, e) {
     var t = {};
     for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p) && e.indexOf(p) < 0)
@@ -23,36 +10,32 @@ var __rest = (this && this.__rest) || function (s, e) {
 import React from 'react';
 import ReactDOM from 'react-dom';
 export function enhanceWithClickOutside(Component) {
-    return /** @class */ (function (_super) {
-        __extends(EnhancedComponent, _super);
-        function EnhancedComponent(props) {
-            var _this = _super.call(this, props) || this;
-            _this.handleClickOutside = _this.handleClickOutside.bind(_this);
-            return _this;
+    return class EnhancedComponent extends React.Component {
+        constructor(props) {
+            super(props);
+            this.handleClickOutside = this.handleClickOutside.bind(this);
         }
-        EnhancedComponent.prototype.componentDidMount = function () {
+        componentDidMount() {
             document.addEventListener('click', this.handleClickOutside, true);
-        };
-        EnhancedComponent.prototype.componentWillUnmount = function () {
+        }
+        componentWillUnmount() {
             document.removeEventListener('click', this.handleClickOutside, true);
-        };
-        EnhancedComponent.prototype.handleClickOutside = function (e) {
-            var domNode = this.__domNode;
+        }
+        handleClickOutside(e) {
+            const domNode = this.__domNode;
             if ((!domNode || !domNode.contains(e.target)) &&
                 this.__wrappedInstance &&
                 typeof this.__wrappedInstance.handleClickOutside === 'function') {
                 this.__wrappedInstance.handleClickOutside(e);
             }
-        };
-        EnhancedComponent.prototype.render = function () {
-            var _this = this;
-            var rest = __rest(this.props, []);
-            return (<Component {...rest} ref={function (c) {
-                _this.__wrappedInstance = c;
+        }
+        render() {
+            const rest = __rest(this.props, []);
+            return (<Component {...rest} ref={(c) => {
+                this.__wrappedInstance = c;
                 // eslint-disable-next-line react/no-find-dom-node
-                _this.__domNode = ReactDOM.findDOMNode(c);
+                this.__domNode = ReactDOM.findDOMNode(c);
             }}/>);
-        };
-        return EnhancedComponent;
-    }(React.Component));
+        }
+    };
 }
