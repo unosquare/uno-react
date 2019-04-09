ef3a18ce5e9ed2f1a1b10a19944dfc97da418c39
Jose Correa <josemanuelcorrea.13@gmail.com>
2019-04-09 11:31:22 -0500

    adding interval parameter to useNow (#56)

--- src/hooks/useNow.ts
+++ src/hooks/useNow.ts
@@ -1,12 +1,12 @@
 import { useEffect, useState } from 'react';
 
-export const useNow = () => {
+export const useNow = (interval = 1000) => {
     const [time, setTime] = useState(new Date());
     let _timer = null;
     const updateTime = () => setTime(new Date());
 
     useEffect(() => {
-        _timer = setInterval(updateTime, 1000);
+        _timer = setInterval(updateTime, interval);
         return (() => clearInterval(_timer));
     }, []);
 
