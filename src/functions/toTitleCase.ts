d9fa46f397834a231dc727b4ff46a2d7c371fa01
Ramiro Flores <Serk352@gmail.com>
2019-05-14 12:35:58 -0500

    adding toTitleCase function

---
+++ src/functions/toTitleCase.ts
@@ -0,0 +1,8 @@
+export function toTitleCase(title: string) {
+    title = title.toLowerCase();
+    const words = title.split(' ');
+    for (let i = 0; i < words.length; i++) {
+        words[i] = words[i].charAt(0).toUpperCase() + words[i].slice(1);
+    }
+    return words.join(' ');
+}
