c43c9346c28f6df4c69596f20d18fb11babe013e
Ramiro Flores <Serk352@gmail.com>
2019-06-10 17:07:29 -0500

    revert

--- src/hoc/ClickOutside.tsx
+++ src/hoc/ClickOutside.tsx
@@ -1,6 +1,54 @@
 import * as React from 'react';
+import ReactDOM from 'react-dom';
 
-export function enhanceWithClickOutside(Component: any, onClickOutside: any) {
+export function enhanceWithClickOutside(Component: any) {
+    return class EnhancedComponent extends React.Component {
+        public __wrappedInstance: any;
+        public __domNode: any;
+
+        constructor(props: any) {
+            super(props);
+            this.handleClickOutside = this.handleClickOutside.bind(this);
+        }
+
+        public componentDidMount() {
+            document.addEventListener('click', this.handleClickOutside, true);
+        }
+
+        public componentWillUnmount() {
+            document.removeEventListener('click', this.handleClickOutside, true);
+        }
+
+        public handleClickOutside(e: { target: any; }) {
+            const domNode = this.__domNode;
+
+            if (
+                (!domNode || !domNode.contains(e.target)) &&
+                this.__wrappedInstance &&
+                typeof this.__wrappedInstance.handleClickOutside === 'function'
+            ) {
+                this.__wrappedInstance.handleClickOutside(e);
+            }
+        }
+
+        public render() {
+            const { ...rest } = this.props;
+
+            return (
+                <Component
+                    {...rest}
+                    ref={(c: React.ReactInstance) => {
+                        this.__wrappedInstance = c;
+                        // eslint-disable-next-line react/no-find-dom-node
+                        this.__domNode = ReactDOM.findDOMNode(c);
+                    }}
+                />
+            );
+        }
+    };
+}
+
+export function enhanceFunctionComponentWithClickOutside(Component: any, onClickOutside: any) {
     return (props: any) => {
         const node: any = React.useRef();
 
