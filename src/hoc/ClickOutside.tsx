055dd89ff3409fb54c1b822ab020d43a77bc3f0c
Ramiro Flores <Serk352@gmail.com>
2019-06-10 17:26:53 -0500

    Issue67 fix enhanceWithClickOutside (#68)
    * working on new enhanceWithClickOutside
    
    * Update ClickOutside.tsx
    
    * revert

--- src/hoc/ClickOutside.tsx
+++ src/hoc/ClickOutside.tsx
@@ -47,3 +47,28 @@ export function enhanceWithClickOutside(Component: any) {
         }
     };
 }
+
+export function enhanceFunctionComponentWithClickOutside(Component: any, onClickOutside: any) {
+    return (props: any) => {
+        const node: any = React.useRef();
+
+        const handleClick = (ev: any) => {
+            if (!node.current.contains(ev.target)) { onClickOutside(); }
+        };
+
+        React.useEffect(() => {
+            document.addEventListener('click', handleClick);
+            return () => document.removeEventListener('click', handleClick);
+        }, []);
+
+        return (
+            <div
+                ref={node}
+            >
+                <Component
+                    {...props}
+                />
+            </div>
+        );
+    };
+}
