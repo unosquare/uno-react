936bbac1bf07c566745742d2f59e2396202e443f
Ramiro Flores <Serk352@gmail.com>
2019-06-11 11:02:41 -0500

    fixing files

--- src/hoc/ClickOutside.tsx
+++ src/hoc/ClickOutside.tsx
@@ -47,28 +47,3 @@ export function enhanceWithClickOutside(Component: any) {
         }
     };
 }
-
-export function useClickOutside(Component: any, onClickOutside: any) {
-    return (props: any) => {
-        const node: any = React.useRef();
-
-        const handleClick = (ev: any) => {
-            if (!node.current.contains(ev.target)) { onClickOutside(); }
-        };
-
-        React.useEffect(() => {
-            document.addEventListener('click', handleClick);
-            return () => document.removeEventListener('click', handleClick);
-        }, []);
-
-        return (
-            <div
-                ref={node}
-            >
-                <Component
-                    {...props}
-                />
-            </div>
-        );
-    };
-}
--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,3 +1,4 @@
+export * from './useClickOutside';
 export * from './useStateForField';
 export * from './useStateForModel';
 export * from './useStateForModelWithLoading';
