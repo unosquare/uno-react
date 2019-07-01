5220f328b6fda2c7f4b7c226bd3731e49bd88d88
Leonardi Pérez <marcolpr98@gmail.com>
2019-07-01 16:36:26 -0500

    Changed validateEndDate

--- src/functions/validations.ts
+++ src/functions/validations.ts
@@ -26,7 +26,7 @@ ValidatorForm.addValidationRule('isNotAllBlanks', isNotAllBlanks);
 
 ValidatorForm.addValidationRule('maxNaturalNumber', maxNaturalNumber);
 
-function validateEndDate(startValue: any, endValue: any) {
+function validateEndDate(endValue: any, startValue: any) {
     const startDate = parse(startValue);
     const endDate = parse(endValue);
     return (differenceInMinutes(endDate, startDate) > 0);
