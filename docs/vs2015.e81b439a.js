5f9ce170570bfe665002bc72c91b117e6466c5e7
Leonardi Pérez <marcolpr98@gmail.com>
2019-07-30 09:39:57 -0500

    Fixed navbar and updated uno-material-ui

--- docs/vs2015.e81b439a.js
+++ docs/vs2015.e81b439a.js
@@ -218,7 +218,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "61033" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "54655" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
