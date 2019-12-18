61ea8a8ba4824c1bdddd25f80af061a5bbfa6203
Geo Perez <1775792+geoperez@users.noreply.github.com>
2019-12-18 09:50:43 -0600

    [FIX] Typing

--- src/hooks/useToggle.ts
+++ src/hooks/useToggle.ts
@@ -1,6 +1,6 @@
 import * as React from 'react';
 
-export function useToggle(defaultValue: boolean): [boolean, React.Dispatch<boolean>] {
+export function useToggle(defaultValue: boolean): [boolean, () => void] {
     const [getField, setField] = React.useState(defaultValue);
 
     const toggleField = (): void => setField(!getField);
