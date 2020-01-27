20608d004891b6afa6acca78d57bab9b592ae80b
JCorrea13 <josemanuelcorrea.13@gmail.com>
2020-01-26 20:41:45 -0600

    adding useValidation

---
+++ src/hooks/useValidation.ts
@@ -0,0 +1,24 @@
+import { useEffect, useState } from 'react';
+
+export const useValidation = 
+    (fn: (string, any, {}) => string , value: any) => {
+    
+    const [errors, setErrors] = useState({});
+    const [isValid, setIsValid] = useState(true);
+  
+    useEffect(() => {
+      const errors = Object.keys(value).reduce(
+        (last, current) => {
+          last[current] = fn(current, value[current], value);
+          return last;
+        }, {});
+  
+      const _isValid = Object.keys(errors).some(x => errors[x]);
+  
+      setErrors(errors);
+      setIsValid(_isValid);
+  
+    }, [value]); 
+
+    return [errors, isValid];
+};
