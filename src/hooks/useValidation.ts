4cf01b54f152126d7ffb237df885b63eea411278
JCorrea13 <josemanuelcorrea.13@gmail.com>
2020-01-27 18:22:50 -0600

    adding initial status

--- src/hooks/useValidation.ts
+++ src/hooks/useValidation.ts
@@ -1,24 +1,24 @@
 import { useEffect, useState } from 'react';
 
-export const useValidation = 
-    (fn: (string, any, {}) => string , value: any) => {
-    
+export const useValidation = (fn: (string, any, {}) => string , value: any): [boolean, {}] => {
+    const [initialValue, _] = useState(value);
     const [errors, setErrors] = useState({});
     const [isValid, setIsValid] = useState(true);
   
     useEffect(() => {
-      const errors = Object.keys(value).reduce(
-        (last, current) => {
-          last[current] = fn(current, value[current], value);
+      const errors = Object.keys(value).reduce((last, current) => {
+          const hasChanged = initialValue[current] != value[current];
+          last[current] = !hasChanged ? '' : fn(current, value[current], value);
+
           return last;
         }, {});
-  
+
       const _isValid = !Object.keys(errors).some(x => errors[x]);
-  
+
       setErrors(errors);
       setIsValid(_isValid);
   
     }, [value]); 
 
-    return [errors, isValid];
+    return [isValid, errors];
 };
