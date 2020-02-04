576dae65a1f833d14faed0c53117b596741e3677
AnyxLeo <anyx.leo@gmail.com>
2020-02-04 11:46:43 -0600

    Returning Type

--- src/hooks/useValidation.ts
+++ src/hooks/useValidation.ts
@@ -2,37 +2,32 @@ import { useEffect, useState } from 'react';
 import { useStateForModel } from './useStateForModel';
 
 export const useValidation = (
-  fn: (propName: string, prop: any, model: {}) => string,
-  value: Record<string, any>
-) => {
-  const [errors, setErrors] = useState<Record<string, string>>({});
-  const [isValid, setIsValid] = useState(true);
-  const [model, setModel] = useStateForModel(value);
-  const [flags, setFlags] = useState<Record<string, boolean>>({});
+    fn: (propName: string, prop: any, model: {}) => string,
+    value: Record<string, any>,
+): [Record<string, string>, boolean] => {
+    const [errors, setErrors] = useState<Record<string, string>>({});
+    const [isValid, setIsValid] = useState(true);
+    const [model, setModel] = useStateForModel(value);
+    const [flags, setFlags] = useState<Record<string, boolean>>({});
 
-  useEffect(() => {
-    const errors = Object.keys(value).reduce(
-      (last: Record<string, string>, current: string) => {
-        if (model[current] !== value[current] || flags[current]) {
-          flags[current] = true;
-          last[current] = fn(current, value[current], value);
-        } else {
-          flags[current] = false;
-        }
-        return last;
-      },
-      {}
-    );
+    useEffect(() => {
+        const errors = Object.keys(value).reduce((last: Record<string, string>, current: string) => {
+            if (model[current] !== value[current] || flags[current]) {
+                flags[current] = true;
+                last[current] = fn(current, value[current], value);
+            } else {
+                flags[current] = false;
+            }
+            return last;
+        }, {});
 
-    setFlags(flags);
-    
-    const _isValid =
-      !Object.keys(errors).some(x => errors[x]) &&
-      !Object.keys(flags).some(x => flags[x] === false);
+        setFlags(flags);
 
-    setErrors(errors);
-    setIsValid(_isValid);
-  }, [value]);
+        const _isValid = !Object.keys(errors).some(x => errors[x]) && !Object.keys(flags).some(x => flags[x] === false);
 
-  return [errors, isValid];
+        setErrors(errors);
+        setIsValid(_isValid);
+    }, [value]);
+
+    return [errors, isValid];
 };
