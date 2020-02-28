9b6562db3c825e0c82770ba99430e5037733beb0
Geo Perez <1775792+geoperez@users.noreply.github.com>
2020-02-28 10:29:09 -0600

    Feature: FormStatus

--- src/hooks/useApiForm.ts
+++ src/hooks/useApiForm.ts
@@ -1,42 +1,47 @@
 import * as React from 'react';
 import { useStateForModel } from './useStateForModel';
-import { useToggle } from './useToggle';
 import { useValidation } from './useValidation';
+
+export enum FormStatus {
+    Valid,
+    Loading,
+    ErrorLoading,
+    ErrorValidation,
+}
 
 export function useApiFormWithValidation<T>(
     dataSource: string | Request | (() => Promise<Response>),
     validation: (propName: string, prop: any, model: {}) => string,
     transform?: (responseObject: object) => object,
-): [T, (event: any) => void, boolean, boolean, boolean, Record<string, string>] {
+): [T, (event: any) => void, FormStatus, Record<string, string>] {
     const [getter, setter] = useStateForModel<T>({} as T);
-    const [isLoading, toggleIsLoading] = useToggle(true);
-    const [error, toggleError] = useToggle(false);
+    const [status, setStatus] = React.useState(FormStatus.Valid);
     const [isValid, errors] = useValidation(validation, getter, 100, true);
 
     React.useEffect(() => {
+        setStatus(FormStatus.Loading);
         const data = typeof dataSource === 'function' ? dataSource() : fetch(dataSource);
 
         data.then((response: Response) => {
             if (response.ok) {
                 response.json().then((y: any) => {
                     setter(transform ? transform(y) : y);
-                    toggleIsLoading();
+                    setStatus(isValid ? FormStatus.Valid : FormStatus.ErrorValidation);
                 });
             } else {
-                toggleError();
-                toggleIsLoading();
+                setStatus(FormStatus.ErrorLoading);
             }
         });
     }, []);
 
-    return [getter, setter, isLoading, error, isValid, errors];
+    return [getter, setter, isValid ? status : FormStatus.ErrorValidation, errors];
 }
 
 export function useApiForm<T>(
     dataSource: string | Request | (() => Promise<Response>),
     transform?: (responseObject: object) => object,
 ): [T, (event: any) => void, boolean, boolean] {
-    const [getter, setter, isLoading, error] = useApiFormWithValidation<T>(dataSource, () => '', transform);
+    const [getter, setter, status] = useApiFormWithValidation<T>(dataSource, () => '', transform);
 
-    return [getter, setter, isLoading, error];
+    return [getter, setter, status === FormStatus.Loading, status == FormStatus.ErrorLoading];
 }
