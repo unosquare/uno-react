dff28c16f5174a5e766d4efd333aa43755f85418
Geovanni Perez <1775792+geoperez@users.noreply.github.com>
2020-04-06 11:20:50 -0500

    Delete test.ts

--- test.ts
+++
@@ -1,8 +0,0 @@
-const valueToResponse = (value: any): Promise<Response> =>
-    new Promise<Response>(resolve => resolve({ ok: true, json: () => new Promise(r => r(value)) } as Response));
-
-const c = (): Promise<Response> => valueToResponse('HOLA');
-
-c().then(r => {
-    if (r.ok) r.json().then(persona => console.log(persona));
-});
