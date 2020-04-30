1dcc0defc69c77de6054ff93b2db2f682db15e3b
Geo Perez <geovanni.perez@gmail.com>
2020-04-30 11:39:22 -0500

    Feat: new hook

--- src/hooks/useUrlId.ts
+++ src/hooks/useUrlId.ts
@@ -1,8 +1,10 @@
 import * as React from 'react';
 import { getQueryParam } from 'uno-js';
 
-export const useUrlId = (idName: string, window: any): [string | number | null, () => void, () => void] => {
-    const [getId, setId] = React.useState<string | number | null>(null);
+export type key = string | number | null;
+
+export const useUrlId = (idName: string, window: any): [key, (value: key) => void, () => void, () => void] => {
+    const [getId, setId] = React.useState<key>(null);
 
     React.useEffect(() => {
         setId(getQueryParam(idName, window.location.href));
@@ -12,5 +14,5 @@ export const useUrlId = (idName: string, window: any): [string | number | null, 
     const reset = (): void => window.history.pushState('', '', getBaseUrl());
     const complete = (): void => window.history.pushState('', '', `${getBaseUrl()}?${idName}=${getId}`);
 
-    return [getId, reset, complete];
+    return [getId, setId, reset, complete];
 };
