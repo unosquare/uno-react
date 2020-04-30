c9af3fbfb084d41115aee1ab398b4be9916dc2fc
AnyxLeo <anyx.leo@gmail.com>
2020-04-30 10:52:28 -0500

    useUrlId

---
+++ src/hooks/useUrlId.ts
@@ -0,0 +1,16 @@
+import * as React from 'react';
+import { getQueryParam } from 'uno-js';
+
+export const useUrlId = (idName: string, window: any) => {
+    const [getId, setId] = React.useState(null);
+
+    React.useEffect(() => {
+        setId(getQueryParam(idName, window.location.href));
+    }, [idName, window.location]);
+
+    const getBaseUrl = () => window.location.href.split('?')[0];
+    const reset = () => window.history.pushState('', '', getBaseUrl());
+    const complete = () => window.history.pushState('', '', `${getBaseUrl()}?${idName}=${getId}`);
+
+    return [getId, setId, reset, complete];
+};
