f74ae52a821ef00f159ba2600c196cec5375432a
Geo Perez <geovanni.perez@gmail.com>
2020-06-17 13:28:29 -0500

    Add Generic Type

--- src/functions/valueToResponse.ts
+++ src/functions/valueToResponse.ts
@@ -1,7 +1,9 @@
-export const valueToResponse = (value: Record<string, unknown>): Promise<Response> =>
+export const valueToResponseType = <T>(value: T): Promise<Response> =>
     new Promise<Response>((resolve) =>
         resolve({
             ok: true,
             json: () => new Promise((r) => r(value)),
         } as Response),
     );
+
+export const valueToResponse = (value: Record<string, unknown>): Promise<Response> => valueToResponseType(value);
