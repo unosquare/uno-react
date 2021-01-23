961c5d25865c87860e32ade1b57060ac06141650
Noel Camacho <ncamacho0@ucol.mx>
2021-01-23 13:27:27 -0600

    lint

--- src/hooks/usePersistedState.test.ts
+++ src/hooks/usePersistedState.test.ts
@@ -2,18 +2,18 @@ import { act, renderHook } from '@testing-library/react-hooks';
 import { usePersistedState } from './usePersistedState';
 
 test('should save default value in LocalStatorage ', () => {
-    const key: string = 'exampleToken';
-    const defaultValue: string = 'unosquareToken';
+    const key = 'exampleToken';
+    const defaultValue = 'unosquareToken';
     const { result } = renderHook(() => usePersistedState(defaultValue, key));
 
     expect(result.current[0]).toBe(defaultValue);
 });
 
 test('should save in LocalStatorage, changing default value ', () => {
-    const key: string = 'exampleToken';
-    const defaultValue: string = 'unosquareToken';
+    const key = 'exampleToken';
+    const defaultValue = 'unosquareToken';
     const { result } = renderHook(() => usePersistedState(defaultValue, key));
-    const value: string = '123456789';
+    const value = '123456789';
     act(() => {
         result.current[1](value);
     });
