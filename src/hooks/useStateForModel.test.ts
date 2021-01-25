829ca4aed65803ba8bbb999c5a061e4a5ade9f35
Noel Camacho <ncamacho0@ucol.mx>
2021-01-24 19:27:31 -0600

    version

--- src/hooks/useStateForModel.test.ts
+++ src/hooks/useStateForModel.test.ts
@@ -28,3 +28,31 @@ test('Should keep updated the values of a model that are related to an input(eve
 
     expect(result.current[0]).toStrictEqual(initialValue);
 });
+
+test('Should keep updated the values of a model that are related to an input(event) handling the inputs onChange calls adding new property if does not exist.', () => {
+    const initialValue = {
+        id: 1,
+        name: 'John',
+        lastName: 'Doe',
+    };
+    const { result } = renderHook(() => useStateForModel(initialValue));
+
+    const eventInputName = {
+        target: { value: 'value name', name: 'name' },
+    } as React.ChangeEvent<HTMLInputElement>;
+
+    const eventInputLastName = {
+        bubbles: false,
+    } as React.ChangeEvent<HTMLInputElement>;
+
+    act(() => {
+        result.current[1](eventInputName);
+    });
+    act(() => {
+        result.current[1](eventInputLastName);
+    });
+    initialValue.name = 'value name';
+    initialValue['bubbles'] = false;
+
+    expect(result.current[0]).toStrictEqual(initialValue);
+});
